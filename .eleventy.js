9e394568f4e88ecee0b333a5707e9aaddf0fba03
Julien Taquet <julientaq@mac16.local>
2022-12-22 11:28:02 +0100

    fix preview

--- .eleventy.js
+++ .eleventy.js
@@ -1,143 +1,168 @@
 // const cheerio = require("cheerio");
 
-const Cache = require("@11ty/eleventy-cache-assets");
-const pluginTOC = require("eleventy-plugin-nesting-toc");
-const markdownIt = require("markdown-it");
-const markdownItAnchor = require("markdown-it-anchor");
-const { DateTime } = require("luxon");
-const cheerio = require('cheerio');
+const Cache = require('@11ty/eleventy-cache-assets')
+const pluginTOC = require('eleventy-plugin-nesting-toc')
+const markdownIt = require('markdown-it')
+const markdownItAnchor = require('markdown-it-anchor')
+const { DateTime } = require('luxon')
+const cheerio = require('cheerio')
 const flatten = require('flat')
 const fs = require('fs')
-const fg = require('fast-glob');
+const fg = require('fast-glob')
+const path = require('path')
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
 
-
-module.exports = function (eleventyConfig) {
+module.exports = function(eleventyConfig) {
   // collection
 
+  eleventyConfig.addFilter('imagesHandler', function(value, id) {
+    if (fs.existsSync('public/images/articles')) {
+      return
+    }
+    fs.mkdir(path.join(__dirname, 'public/images/articles'), (err) => {
+      if (err) {
+        return console.error(err)
+      }
+      console.log('Directory created successfully!')
+    })
+    const article = new JSDOM(value)
+    article.window.document.body.querySelectorAll('img').forEach((img) => {
+      https
+        .get(img.src, (res) => {
+          const file = fs.createWriteStream(
+            `public/images/articles/art${id}-${img.alt}`
+          )
+          res.pipe(file)
+          file.on('finish', () => {
+            file.close()
+            console.log(`${img.src} has been downloaded!`)
+          })
+        })
+        .on('error', (err) => {
+          console.log('Error: ', err.message)
+        })
+      img.src = `/images/articles/art${id}-${img.alt}`
+    })
+    return article.window.document.body.innerHTML
+    // return value
+  })
 
-
-
-
-  eleventyConfig.addCollection("sortedByOrder", function (collectionApi) {
+  eleventyConfig.addCollection('sortedByOrder', function(collectionApi) {
     return collectionApi.getAll().sort((a, b) => {
+      if (a.data.order > b.data.order) return 1
+      else if (a.data.order < b.data.order) return -1
+      else return 0
+    })
+  })
 
-      if (a.data.order > b.data.order) return 1;
-      else if (a.data.order < b.data.order) return -1;
-      else return 0;
-    });
-  });
-
-
-  eleventyConfig.addPassthroughCopy({ "static/css": "/css" });
-  eleventyConfig.addPassthroughCopy({ "static/fonts": "/fonts" });
-  eleventyConfig.addPassthroughCopy({ "static/js": "/js" });
-  eleventyConfig.addPassthroughCopy({ "static/images": "/images" });
-  eleventyConfig.addPassthroughCopy({ "static/outputs": "/outputs" });
-  eleventyConfig.addPassthroughCopy({ "static/admin": "/admin" });
+  eleventyConfig.addPassthroughCopy({ 'static/css': '/css' })
+  eleventyConfig.addPassthroughCopy({ 'static/fonts': '/fonts' })
+  eleventyConfig.addPassthroughCopy({ 'static/js': '/js' })
+  eleventyConfig.addPassthroughCopy({ 'static/images': '/images' })
+  eleventyConfig.addPassthroughCopy({ 'static/outputs': '/outputs' })
+  eleventyConfig.addPassthroughCopy({ 'static/admin': '/admin' })
 
   // plugin TOC
-  eleventyConfig.addPlugin(pluginTOC);
+  eleventyConfig.addPlugin(pluginTOC)
   eleventyConfig.setLibrary(
-    "md",
+    'md',
     markdownIt({
       html: true,
       linkify: true,
       typographer: true,
     }).use(markdownItAnchor, {})
-  );
+  )
+
+  // eleventyConfig.addFilter('getcontent', function(value) {
+  //   const article = new JSDOM(value)
+  //
+  //   return article.window.document.body.querySelectorAll('.content').innerHTML
+  // })
 
   // useful to use the toc somewhere else
-  eleventyConfig.addFilter("prependLinks", function (value, prepend) {
+  eleventyConfig.addFilter('prependLinks', function(value, prepend) {
     return value.replace(/<a href="/g, `<a href="${prepend}`)
-  });
-  eleventyConfig.addFilter("replaceWithRegex", function (replaceThat, replaceWith) {
-    let regex = new RegExp(replaceThat);
-    return value.replace(regex, replaceWith)
-  });
+  })
+  eleventyConfig.addFilter(
+    'replaceWithRegex',
+    function(replaceThat, replaceWith) {
+      let regex = new RegExp(replaceThat)
+      return value.replace(regex, replaceWith)
+    }
+  )
 
-  eleventyConfig.addFilter("cleanLink", function (value) {
-    let regex = new RegExp('');
-    return value.replace(/static\/outputs\/\d+?\//, "")
-  });
+  eleventyConfig.addFilter('cleanLink', function(value) {
+    let regex = new RegExp('')
+    return value.replace(/static\/outputs\/\d+?\//, '')
+  })
 
   // add latin number plugin
-  eleventyConfig.addFilter("romanize", function (value) {
-    return romanize(value);
-  });
+  eleventyConfig.addFilter('romanize', function(value) {
+    return romanize(value)
+  })
 
-// add link to the diverses files
-  const supplementary = fg.sync(['**/outputs/**', '!**/node_modules/', '!**/temp', '!**/public',]);
-  
-  eleventyConfig.addCollection('supplementaryFiles', function (collection) {
-    return supplementary;
-  });
+  // add link to the diverses files
+  const supplementary = fg.sync([
+    '**/outputs/**',
+    '!**/node_modules/',
+    '!**/temp',
+    '!**/public',
+  ])
+
+  eleventyConfig.addCollection('supplementaryFiles', function(collection) {
+    return supplementary
+  })
 
   // \get the date with luxon (for all date)
-  eleventyConfig.addFilter("postDate", (dateObj) => {
+  eleventyConfig.addFilter('postDate', (dateObj) => {
     let date = new Date(dateObj)
-    return DateTime.fromJSDate(date).toLocaleString(DateTime.DATE_MED);
-  });
-
+    return DateTime.fromJSDate(date).toLocaleString(DateTime.DATE_MED)
+  })
 
   // limit the amount of items
-  eleventyConfig.addFilter("limit", function (arr, limit) {
-    return arr.slice(0, limit);
-  });
+  eleventyConfig.addFilter('limit', function(arr, limit) {
+    return arr.slice(0, limit)
+  })
 
-  eleventyConfig.addFilter("filterContent", function (value, el) {
+  eleventyConfig.addFilter('filterContent', function(value, el) {
     // console.log(value);
-    const $ = cheerio.load(value);
+    const $ = cheerio.load(value)
     if ($.html(el)) {
-      return value = $.html(el);
+      return (value = $.html(el))
+    } else {
+      return value
     }
-    else {
-      return value;
-    }
-  });
+  })
 
+  eleventyConfig.addFilter('addIDtoTitles', function(value) {
+    // cheerio only accept string, no html buffer :shrug:
+    const $ = cheerio.load(`${value}`)
 
-  eleventyConfig.addFilter("addIDtoTitles", function (value) {
-    // cheerio only accept string, no html buffer :shrug: 
-    const $ = cheerio.load(`${value}`);
-
-    $('h2,h3,h4,h5').each(function (i, elem) {
-      $(this).attr('id', $(this).text().toLowerCase().replace(/\s/g, ''));
+    $('h2,h3,h4,h5').each(function(i, elem) {
+      $(this).attr('id', $(this).text().toLowerCase().replace(/\s/g, ''))
     })
-    
+
     // add it to figure, seems to break because of the base64
 
     // doesnt work with base64 as the content is way too big
     // $('figure').each(function (i, elem) {
     //   $(this).attr('id', `fig-${i})`);
     // })
-    return $.html();
-
-
+    return $.html()
   })
 
-
-
-
-
-
-  eleventyConfig.addFilter('showAvailableMeta', function (value) {
+  eleventyConfig.addFilter('showAvailableMeta', function(value) {
     return propertiesToArray(value)
   })
 
-
-
   eleventyConfig.addPlugin(pluginTOC, {
-    tags: ["h2", "h3", "h4"], // which heading tags are selected headings must each have an ID attribute
-    wrapper: "nav", // element to put around the root `ol`/`ul`
-    wrapperClass: "toc", // class for the element around the root `ol`/`ul`
+    tags: ['h2', 'h3', 'h4'], // which heading tags are selected headings must each have an ID attribute
+    wrapper: 'nav', // element to put around the root `ol`/`ul`
+    wrapperClass: 'toc', // class for the element around the root `ol`/`ul`
     ul: false, // if to use `ul` instead of `ol`
     flat: false,
-  });
-
-
-
-
-
+  })
 
   // folder structures
   // -----------------------------------------------------------------------------
@@ -146,49 +171,70 @@ module.exports = function (eleventyConfig) {
   // -----------------------------------------------------------------------------
   return {
     dir: {
-      input: "src",
-      output: "public",
-      includes: "layouts",
-      data: "data",
+      input: 'src',
+      output: 'public',
+      includes: 'layouts',
+      data: 'data',
     },
-  };
-};
-
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+  }
 }
 
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min
+}
 
 function romanize(num) {
   // taken from Steven Levithan
   // https://blog.stevenlevithan.com/archives/javascript-roman-numeral-converter
 
-  if (isNaN(num))
-    return NaN;
-  var digits = String(+num).split(""),
-    key = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM",
-      "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC",
-      "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
-    roman = "",
-    i = 3;
-  while (i--)
-    roman = (key[+digits.pop() + (i * 10)] || "") + roman;
-  return Array(+digits.join("") + 1).join("M") + roman;
+  if (isNaN(num)) return NaN
+  var digits = String(+num).split(''),
+    key = [
+      '',
+      'C',
+      'CC',
+      'CCC',
+      'CD',
+      'D',
+      'DC',
+      'DCC',
+      'DCCC',
+      'CM',
+      '',
+      'X',
+      'XX',
+      'XXX',
+      'XL',
+      'L',
+      'LX',
+      'LXX',
+      'LXXX',
+      'XC',
+      '',
+      'I',
+      'II',
+      'III',
+      'IV',
+      'V',
+      'VI',
+      'VII',
+      'VIII',
+      'IX',
+    ],
+    roman = '',
+    i = 3
+  while (i--) roman = (key[+digits.pop() + i * 10] || '') + roman
+  return Array(+digits.join('') + 1).join('M') + roman
 }
-
-
-
 
 // to debug → get all the property of an element as a flat HTML
 
-
 function propertiesToArray(value) {
-  let stuff = flatten(value, { maxDepth: 10 });
-  let content = "";
+  let stuff = flatten(value, { maxDepth: 10 })
+  let content = ''
   for (var key in stuff) {
-    content += `<section><div class="meta">${key}</div><div class="value">${stuff[key] != null ? stuff[key] : ''}</div></section>`
+    content += `<section><div class="meta">${key}</div><div class="value">${stuff[key] != null ? stuff[key] : ''
+      }</div></section>`
   }
-  return content;
-
+  return content
 }
--- src/data/articleQuery.js
+++ src/data/articleQuery.js
@@ -1,102 +1,82 @@
 // required packages
-const axios = require('axios');
+const axios = require('axios')
 const config = require('./config.js')
-const { AssetCache } = require("@11ty/eleventy-cache-assets");
-
-const GRAPHQL_URL = config.url;
-
-
-
+const { AssetCache } = require('@11ty/eleventy-cache-assets')
+const path = require('path')
+const GRAPHQL_URL = config.url
 
 const getData = async () => {
+  let cachedArticles = new AssetCache(`articles`)
 
+  if (cachedArticles.isCacheValid('2d') && config.cache == true) {
+    let data = await cachedArticles.getCachedValue()
+    return { articles: data }
+  } else {
+    const graphqlQuery = async ({ query }) => {
+      const response = await axios.post(GRAPHQL_URL, {
+        query: query,
+      })
 
-	let cachedArticles = new AssetCache(`articles`);
+      if (response.status == 200) {
+        return response.data.data
+      }
+      // Todo: fallback for failure
+    }
 
-	if (cachedArticles.isCacheValid("2d") && config.cache == true) {
-		let data = await (cachedArticles.getCachedValue())
-		return { articles: data };
-	}
-	else {
+    const data = await graphqlQuery({
+      query: `{
+  manuscriptsPublishedSinceDate(startDate: 1624101179050, limit: null) {
+  id
+  shortId
+  files {
+    id
+    name
+    alt
+    caption
+    tags
+    objectId
+    storedObjects {
+      type
+      key
+      size
+      mimetype
+      extension
+      imageMetadata {
+        width
+        height
+        space
+        density
+      }
+      url
+    }
+    uploadStatus
+    inUse
+  }
+  status
+  meta {
+    title
+    source
+    abstract
+  }
+  submission
+  publishedDate
+  printReadyPdfUrl
+  styledHtml
+  css
+   }
+}`,
+    })
+    // This is being done because "submission" comes in as stringified JSON which needs to be unstringified to use in templates
+    const parsedArticles = data.manuscriptsPublishedSinceDate.map((article) => {
+      // console.log(article)
+      return { parsedSubmission: JSON.parse(article.submission), ...article }
+    })
 
-		const graphqlQuery = async ({ query }) => {
-			const response = await axios.post(GRAPHQL_URL, {
-				query: query,
-			});
+    await cachedArticles.save(parsedArticles, 'json')
+    return {
+      articles: parsedArticles,
+    }
+  }
+}
 
-			if (response.status == 200) {
-				return response.data.data;
-			}
-			// Todo: fallback for failure
-		};
-
-		const data = await graphqlQuery({
-			query: `query {
-			manuscriptsPublishedSinceDate(startDate: 1624101179050, limit: null) {
-				id
-				shortId
-				files {
-					# attachments
-					id
-					label
-					fileType
-					filename
-					url
-					mimeType
-					size
-				}
-				status
-				meta {
-					
-					title
-					source
-					articleType
-					declarations {
-						openData
-						openPeerReview
-						preregistered
-						previouslySubmitted
-						researchNexus
-						streamlinedReview
-					}
-					articleSections
-					articleIds {
-						pubIdType
-						id
-					}
-					abstract
-					subjects
-					history {
-						type
-						date
-					}
-					publicationDates {
-						type
-						date
-					}
-					notes {
-						notesType
-						content
-					}
-					keywords
-					manuscriptId
-				}
-				submission 
-				publishedDate
-			}
-		}     
-      `,
-		});
-		// This is being done because "submission" comes in as stringified JSON which needs to be unstringified to use in templates
-		const parsedArticles = data.manuscriptsPublishedSinceDate.map((article) => {
-			return { parsedSubmission: JSON.parse(article.submission), ...article };
-		});
-
-		await cachedArticles.save(parsedArticles, "json")
-		return {
-			articles: parsedArticles,
-		};
-	}
-};
-
-module.exports = getData;
+module.exports = getData
