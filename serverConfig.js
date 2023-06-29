563cf63da0e6bd992d8948111fbb4b7ecabfc64e
Pankaj Agrawal <pankaj@coloredcow.com>
2023-06-29 21:44:14 +0530

    fix(*): added env var

--- serverConfig.js
+++ serverConfig.js
@@ -1,16 +1,14 @@
 const getConfig = () => {
-  let baseUrl = process.env.FLAX_CLIENT_API_URL
-    ? process.env.FLAX_CLIENT_API_URL
-    : "http://client:4000";
+	let baseUrl = process.env.FLAX_CLIENT_API_URL
+		? process.env.FLAX_CLIENT_API_URL
+		: "URL NOT FOUND";
 
-  baseUrl = baseUrl.replace(/\/+$/, "");
+	baseUrl = baseUrl.replace(/\/+$/, "");
 
-  baseUrl = "https://a33b-111-223-30-157.ngrok-free.app";
-
-  return {
-    baseUrl: baseUrl,
-    apiUrl: `${baseUrl}/graphql`,
-  };
+	return {
+		baseUrl: baseUrl,
+		apiUrl: `${baseUrl}/graphql`,
+	};
 };
 
 module.exports = { getConfig };
