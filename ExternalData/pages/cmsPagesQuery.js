04677823acae8e063e55f8728c65af697803bef2
Pankaj Agrawal <pankaj@coloredcow.com>
2023-07-05 21:04:43 +0530

    fixed updates

--- ExternalData/pages/cmsPagesQuery.js
+++ ExternalData/pages/cmsPagesQuery.js
@@ -1,20 +1,6 @@
 const { makeAPICall } = require("../../api");
 const fs = require("fs");
-
 const dataFile = `src/data/cmsPages.json`;
-
-const cleanMeta = (cmsPage) => {
-	let pageMeta = cmsPage.meta;
-	let updatedMeta = {};
-	if (!pageMeta) {
-		return updatedMeta;
-	}
-	if (typeof pageMeta != "object") {
-		updatedMeta = JSON.parse(pageMeta);
-	}
-	cmsPage.meta = updatedMeta;
-	return updatedMeta;
-};
 
 const getPages = async () => {
 	let graphQLQuery = JSON.stringify({
@@ -40,17 +26,13 @@ const getPages = async () => {
 	}
 
 	let cmsPagesData = response.cmsPages;
-	let pageShortCodes = {};
 	let cmsPages = [];
 	for (let i in cmsPagesData) {
 		let cmsPage = cmsPagesData[i];
-		cleanMeta(cmsPage);
-		pageShortCodes[cmsPage.shortcode] = cmsPage;
 		cmsPages.push(cmsPage);
 	}
 
 	return {
-		shortCodePages: pageShortCodes,
 		pages: cmsPages,
 	};
 };
