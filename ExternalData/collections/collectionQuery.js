9d4ae58c7816757d6006a616b7a81aaf45b6e709
Julien Taquet <julientaq@mac16.local>
2024-03-06 23:23:01 +0100

    collection empty and gumpstation layout

--- ExternalData/collections/collectionQuery.js
+++ ExternalData/collections/collectionQuery.js
@@ -1,9 +1,9 @@
-const { getGroupDataDir, getGroupAssetDir, downloadFile, imageFileLocalUrl} = require("../../helpers");
+const { getGroupDataDir, getGroupAssetDir, downloadFile, imageFileLocalUrl } = require("../../helpers");
 const { getCollectionsQuery } = require('../../queries')
 const fs = require("fs");
 
 const storeImages = async (group, hexCode, collections) => {
-    const collectionImagesDir = getGroupAssetDir(group, hexCode, "images/collections");
+	const collectionImagesDir = getGroupAssetDir(group, hexCode, "images/collections");
 	let updatedCollectionsData = [];
 
 	if (!fs.existsSync(collectionImagesDir)) {
@@ -19,26 +19,30 @@ const storeImages = async (group, hexCode, collections) => {
 		let collection = collections[i];
 
 		if (collection.formData.file) {
-            let imageFullPath = `${collectionImagesDir}/${collection.formData.file.name}`
-	        let imageLocalUrl = imageFileLocalUrl(hexCode, `collections/${collection.formData.file.name}`)
-            downloadFile(collection.formData.image, imageFullPath) 
-            collection.formData.image = imageLocalUrl
-        }
+			let imageFullPath = `${collectionImagesDir}/${collection.formData.file.name}`
+			let imageLocalUrl = imageFileLocalUrl(hexCode, `collections/${collection.formData.file.name}`)
+			downloadFile(collection.formData.image, imageFullPath)
+			collection.formData.image = imageLocalUrl
+		}
 
-        updatedCollectionsData.push(collection);
+		updatedCollectionsData.push(collection);
 	}
 
 	return updatedCollectionsData;
 }
 
 const syncData = async (group, cmsLayout) => {
-    const { hexCode } = cmsLayout
+	const { hexCode } = cmsLayout
 	const dataFile = getGroupDataDir(group) + "/cmsCollections.json";
 	let data = await getCollectionsQuery(group)
 
-    collections = await storeImages(group, hexCode, data.cmsMetadata)
+	collections = await storeImages(group, hexCode, data.cmsMetadata)
 	if (collections.length) {
 		fs.writeFileSync(dataFile, JSON.stringify({ collections }), "utf8");
+	}
+	else {
+		// else we should keep the file set in /src/kotahi/data
+		fs.writeFileSync(dataFile, `{ "collections" : [] }`, "utf8");
 	}
 };
 
---
+++ src/kotahi/content/articles/issue-single.md
@@ -0,0 +1,9 @@
+---
+pagination:
+  data: cmsCollections.collections
+  size: 1
+  alias: collectionItem
+layout: "collection-single.njk"
+class: "section-latest"
+permalink: "/issues/{{ pagination.pageNumber + 1 }}/index.html"
+---
