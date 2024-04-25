751a6ea6b9d94302ad5ca93f5234cd12ecd55d45
Julien Taquet <julientaq@mac16.local>
2024-04-25 17:55:19 +0200

    header article ok!

--- 11ty-plugins/collections/dateWrangler.js
+++ 11ty-plugins/collections/dateWrangler.js
@@ -1,5 +1,14 @@
 module.exports = function(eleventyConfig) {
+
   eleventyConfig.addFilter("dateWrangler", function(date, language, format) {
+
+    // set date correclty:
+    console.log(date.split(".")[0])
+    console.log(date.split(".")[0].length)
+    if (date.split(".")[0].length != 4) {
+      date = date.split(".").reverse().join("-");
+    }
+    console.log(date)
     let renderedDate = new Date(date);
     const options = {
       month: format === "short" ? "short" : "long",
