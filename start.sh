e9280482d157ebbec3b647f96c3fcfb988dc12da
Yannis Barlas <yannisbarlas@gmail.com>
2024-10-29 13:33:00 +0200

    fix(*): remove wait for it from start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Use wait-for-it to ensure the server is ready
-/usr/local/bin/wait-for-it.sh server:${SERVER_PORT:-3000} --timeout=180 --strict -- true
+# /usr/local/bin/wait-for-it.sh server:${SERVER_PORT:-3000} --timeout=180 --strict -- true
 
 # Now start supervisord to manage all processes
 /usr/bin/supervisord -c /etc/supervisord.conf
